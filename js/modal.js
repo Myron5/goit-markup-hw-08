1db18fc4f961382c4fceb2a8567d1c4b2c31a2ad
unknown <myron.melnyk.kn.2021@lpnu.ua>
2022-10-01 17:35:33 +0300

    адаптив (без фото)

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,24 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    backdrop: document.querySelector("[data-backdrop]"),
-    modal: document.querySelector("[data-modal]"),
-    firstInput: document.querySelector("[data-autofocus]"), 
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('[data-backdrop]'),
+
+    firstInput: document.querySelector('[data-autofocus]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("modal-box--active");
-    if (refs.modal.classList.toggle("modal-box--close")) 
-      setTimeout(() => { refs.backdrop.classList.toggle("backdrop--is-hidden") }, '980');
+    refs.modal.classList.toggle('modal-box--active');
+    if (refs.modal.classList.toggle('modal-box--close'))
+      setTimeout(() => {
+        refs.backdrop.classList.toggle('backdrop--is-hidden');
+      }, '980');
     else {
-      refs.backdrop.classList.toggle("backdrop--is-hidden");
+      refs.backdrop.classList.toggle('backdrop--is-hidden');
       refs.firstInput.focus();
     }
   }
@@ -23,14 +26,13 @@
 
 (() => {
   const refs = {
-    checkBox: document.querySelector("[data-policy]"),
-    submitBtn: document.querySelector("[data-submit-btn]"),
+    checkBox: document.querySelector('[data-policy]'),
+    submitBtn: document.querySelector('[data-submit-btn]'),
   };
 
-  refs.checkBox.addEventListener("change", toggleAttr);
+  refs.checkBox.addEventListener('change', toggleAttr);
 
   function toggleAttr() {
-    refs.submitBtn.toggleAttribute("disabled")
+    refs.submitBtn.toggleAttribute('disabled');
   }
-
 })();
